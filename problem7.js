f1db1054d29b5ca82db86c9655b709d8d37014cb
Deexit Kumar Sharma <deexitkumarsharma@gmail.com>
2022-11-30 19:50:53 +0530

    problem7 update

--- problem7.js
+++ problem7.js
@@ -1,25 +1,32 @@
 let promise1 = new Promise((resolve, reject) => {
   resolve("Promise 1");
 });
-
 let promise2 = new Promise((resolve, reject) => {
-  reject(("Promise 2"));
+  reject("Promise 2");
+  // resolve("Promise 2");
 });
-
 let promise3 = new Promise((resolve, reject) => {
   resolve("Promise 3");
 });
 
-let promisesArray = [promise1, promise2, promise3];
-//console.log(promisesArray);
-
-let Promise_All = (promisesArray) => {
-    Promise.all(promisesArray).then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error + " " + "rejected");
+let promiseAll = (inputPromises) => {
+  let resolvedPromises = [];
+  return new Promise((resolve, reject) => {
+    for (let promise of inputPromises) {
+      promise
+        .then((value) => {
+          resolvedPromises.push(value);
+          if (resolvedPromises.length === inputPromises.length) {
+            resolve(resolvedPromises);
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    }
   });
-}
-Promise_All(promisesArray);
+};
 
+promiseAll([promise1, promise2, promise3])
+  .then((val) => console.log(val))
+  .catch((val) => console.log(`${val} is rejected`));
